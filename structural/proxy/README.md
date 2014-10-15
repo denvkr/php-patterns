f1904f165733b7e04fb155f6e751b35c605a5866
Dzyanis Kuzmenka <dzyanis@gmail.com>
2014-10-15 16:05:38 +0300

    update info about proxy

--- structural/proxy/README.md
+++ structural/proxy/README.md
@@ -1,4 +1,24 @@
 #Proxy
 A proxy, in its most general form, is a class functioning as an interface to something else. The proxy could interface
 to anything: a network connection, a large object in memory, a file,
-or some other resource that is expensive or impossible to duplicate. [More…](http://en.wikipedia.org/wiki/Proxy_pattern)
+or some other resource that is expensive or impossible to duplicate.
+[More…](http://en.wikipedia.org/wiki/Proxy_pattern)
+
+```php
+$math = new Math(); // INIT VERY FAT OBJECT
+echo 'add 2 2 : ' . $math->add(2, 2) . PHP_EOL; // add 2 2 : 4
+echo 'div 2 2 : ' . $math->div(2, 2) . PHP_EOL; // div 2 2 : 1
+echo 'sub 2 2 : ' . $math->sub(2, 2) . PHP_EOL; // sub 2 2 : 0
+echo 'mul 2 2 : ' . $math->mul(2, 2) . PHP_EOL; // mul 2 2 : 4
+
+$math = new MathProxy();
+echo "Proxy:\n"; // Proxy:
+echo 'add 2 2 : ' . $math->add(2, 2) . PHP_EOL; // add 2 2 : 4
+echo 'sub 2 2 : ' . $math->sub(2, 2) . PHP_EOL; // sub 2 2 : 0
+// INIT VERY FAT OBJECT
+echo 'div 2 2 : ' . $math->div(2, 2) . PHP_EOL; // div 2 2 : 4
+echo 'mul 2 2 : ' . $math->mul(2, 2) . PHP_EOL; // mul 2 2 : 4
+```
+
+##Diagram
+![Proxy UML Diagram](diagram.png)
--- structural/proxy/index.php
+++ structural/proxy/index.php
@@ -5,15 +5,16 @@ namespace DesignPatterns\Structural\Proxy;
 include_once 'MathProxy.php';
 include_once 'Math.php';
 
-$math = new Math();
-echo 'add 2 2 : ' . $math->add(2, 2) . PHP_EOL;
-echo 'div 2 2 : ' . $math->div(2, 2) . PHP_EOL;
-echo 'sub 2 2 : ' . $math->sub(2, 2) . PHP_EOL;
-echo 'mul 2 2 : ' . $math->mul(2, 2) . PHP_EOL;
+$math = new Math(); // INIT VERY FAT OBJECT
+echo 'add 2 2 : ' . $math->add(2, 2) . PHP_EOL; // add 2 2 : 4
+echo 'div 2 2 : ' . $math->div(2, 2) . PHP_EOL; // div 2 2 : 1
+echo 'sub 2 2 : ' . $math->sub(2, 2) . PHP_EOL; // sub 2 2 : 0
+echo 'mul 2 2 : ' . $math->mul(2, 2) . PHP_EOL; // mul 2 2 : 4
 
 $math = new MathProxy();
-echo "Proxy:\n";
-echo 'add 2 2 : ' . $math->add(2, 2) . PHP_EOL;
-echo 'sub 2 2 : ' . $math->sub(2, 2) . PHP_EOL;
-echo 'div 2 2 : ' . $math->div(2, 2) . PHP_EOL;
-echo 'mul 2 2 : ' . $math->mul(2, 2) . PHP_EOL;
+echo "Proxy:\n"; // Proxy:
+echo 'add 2 2 : ' . $math->add(2, 2) . PHP_EOL; // add 2 2 : 4
+echo 'sub 2 2 : ' . $math->sub(2, 2) . PHP_EOL; // sub 2 2 : 0
+// INIT VERY FAT OBJECT
+echo 'div 2 2 : ' . $math->div(2, 2) . PHP_EOL; // div 2 2 : 4
+echo 'mul 2 2 : ' . $math->mul(2, 2) . PHP_EOL; // mul 2 2 : 4
